a6e55f47496de48f30ea445bca2e7cd110cc99c0
SriCharan-Koyalkar <92539071+SriCharan-Koyalkar@users.noreply.github.com>
2022-12-12 23:41:13 +0530

    Update versions.tf

--- editor/versions.tf
+++ editor/versions.tf
@@ -15,16 +15,14 @@
  */
 
 terraform {
-  required_version = ">= 0.15.3"
-
+  required_version = ">=0.13.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
+      version = "~> 4.28"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.0"
+      version = "~> 4.28"
     }
   }
-}
